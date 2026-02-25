67d6cad6bb01dad162a3d013858e28910352f8fd
savipavan <savipavan@gmail.com>
2026-02-25 20:58:38 +0530

    Update DemoApplication.java

--- src/main/java/com/demo/DemoApplication.java
+++ src/main/java/com/demo/DemoApplication.java
@@ -10,7 +10,7 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String hello() {
-        return "CI/CD Pipeline Working 🚀";
+        return "Pavan CI/CD Pipeline Working 🚀";
     }
 
     public static void main(String[] args) {
